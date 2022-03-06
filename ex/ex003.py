10f46dc1b57f1af2571ca2927eba09bec5be98bf
Moises Moreira <92399378+moises-moreira@users.noreply.github.com>
2022-03-06 12:55:16 -0300

    operadores aritimeticos

---
+++ ex/ex003.py
@@ -0,0 +1,10 @@
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s = n1 + n2
+su = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('soma {}, subtração {}, multiplicação {}, divisão {:.2f}'.format(s, su, m, d))
+print('Divisão inteira {} e potência {}'.format(di, e))
